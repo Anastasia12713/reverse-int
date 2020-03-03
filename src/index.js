07f70e59de7ffb138cf7fe4462f4129e9ba4ce85
unknown <anastasia28-05@mail.ru>
2020-03-03 22:57:37 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+    }
+    n = String(n);
+    n = n
+        .split("")
+        .reverse()
+        .join("");
+    return n;
+};
